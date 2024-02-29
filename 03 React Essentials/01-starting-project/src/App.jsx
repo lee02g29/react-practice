43d9f75b7f390d36975881a59844831416bed12d
이혜지 <lee02g29@naver.com>
2024-02-29 16:23:52 +0900

    24/02/29 오늘의 리액트 공부 분량

--- 03 React Essentials/01-starting-project/src/App.jsx
+++ 03 React Essentials/01-starting-project/src/App.jsx
@@ -1,8 +1,14 @@
 import { CORE_CONCEPTS } from "./data";
-import Header from "./components/Header.jsx";
+import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+    console.log(selectedButton)
+  }
+
   return (
     <div>
       <Header />
@@ -25,6 +31,16 @@ function App() {
               {...CORE_CONCEPTS[3]}
             />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+            <menu>
+              <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+              <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+              <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+              <TabButton onSelect={() => handleSelect('state ')}>State</TabButton>
+            </menu>
+          Dynamic Content
         </section>
       </main>
     </div>
